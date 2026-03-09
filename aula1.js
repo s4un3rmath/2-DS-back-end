5fc4d3cadc03790262bc5f568cd935aad177cdf6
Matheo <maria.sauner.lacerda@escola.pr.gov.br>
2026-03-09 08:57:04 -0300

    Add files via upload

---
+++ aula1.js
@@ -0,0 +1,14 @@
+let nome = "Matheo Rafael"
+let altura = 1.600
+let idade = 15
+let cabelo = "raspado"
+let vaiavelcabelo = "liso" 
+let genero = "garoto"
+let cor = "branco"
+let hobby = "desenhar"
+let roupa = "preta e confortavel"
+let acessorio = "correntes"
+let acessori = "braceletes"
+
+console.log( "ola mundo, me chamo " + nome + " sou um(a) "+ genero + " tenho " + idade + " anos, tenho cabelo "+ cabelo + 
+ " e " + vaiavelcabelo + ", tenho " + altura + " de altura, um(a) " + genero + " " + cor + " e eu gosto de " + hobby + ", geralmente eu gosto de usar uma roupa " + roupa + " com " + acessorio + " e" + acessori + " :)")
